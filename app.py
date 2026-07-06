f2f80e5a751c08b8b211a5c23936fd1fe7850ccc
FranciscoQuissungo <franciscoquissungo9@gmail.com>
2026-07-06 20:49:27 +0100

    Fix Flask app initialization and main check

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(name)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Fábrica de Vídeos Online!"
 
-if name == "main":
+if __name =="__main__":
     app.run(host="0.0.0.0", port=5000)
